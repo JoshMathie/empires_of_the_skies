0627f841f6487581d9ed9e5616572928ffef8039
Josh Mathie <joshmathie01@gmail.com>
2023-05-18 16:12:14 +0100

    expanded player info, fleet info, counsellorLocations and changed colours enum to const

--- empires_of_the_skies/src/types.ts
+++ empires_of_the_skies/src/types.ts
@@ -6,9 +6,31 @@ export type MapState = {
 };
 export type PlayerInfo = {
   id: string;
-  colour: PlayerColour;
+  colour: (typeof PlayerColour)[keyof typeof PlayerColour];
   ready: boolean;
   resources: Resources;
+  isArchprelate: boolean;
+  counsellorLocations: counsellorLocations;
+  hereticOrOthodox: string;
+  fleetInfo: FleetInfo[];
+  cathedrals: number;
+  palaces: number;
+  victoryPoints: number;
+  heresyTracker: number[];
+  prisoners: number;
+  shipyards: number;
+};
+
+export type FleetInfo = {
+  fleetId: number;
+  location: number[];
+  skyships: number;
+  regiments: number;
+};
+
+export type counsellorLocations = {
+  playerBoard: boolean[];
+  actionBoard: boolean[];
 };
 
 export type Resources = {
@@ -28,14 +50,14 @@ export type Resources = {
   legacyCard: string;
 };
 
-export enum PlayerColour {
-  red = "#E3000F",
-  green = "#478779",
-  yellow = "FFCC00",
-  blue = "51658D",
-  black = "1A1A18",
-  white = "FFFFFF",
-}
+export const PlayerColour = {
+  red: "#E3000F",
+  green: "#478779",
+  yellow: "#FFCC00",
+  blue: "#51658D",
+  black: "#1A1A18",
+  white: "#FFFFFF",
+} as const;
 
 export type TileInfoProps = {
   name: string;
