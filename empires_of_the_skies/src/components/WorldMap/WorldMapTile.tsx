39ab417ebe481bcbe6269406c681d0c88bd15377
Josh Mathie <joshmathie01@gmail.com>
2023-05-18 16:08:47 +0100

    fixed flipping issues

--- empires_of_the_skies/src/components/WorldMap/WorldMapTile.tsx
+++ empires_of_the_skies/src/components/WorldMap/WorldMapTile.tsx
@@ -1,22 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 
 import ReactCardFlip from "react-card-flip";
+// import { useLongPress } from "../../helpers/useLongPress";
+import { useLongPress } from "use-long-press";
 
 //Method for displaying a flippable tile which contains a world map tile image
 export const WorldMapTile = (props: worldMapTileProps) => {
+  const onClick = () => setFlip(true);
+
+  const xPosition = useRef(0);
+  const yPosition = useRef(0);
+  const longPressCallback = useCallback(() => {}, []);
+
+  const longPressEvent = useLongPress(longPressCallback, {
+    cancelOnMovement: true,
+    cancelOutsideElement: true,
+    threshold: 150,
+    onStart: useCallback((event: any) => {
+      xPosition.current = event.clientX;
+      yPosition.current = event.clientY;
+    }, []),
+  });
+
+  const bind = longPressEvent("test context");
+
   const [flip, setFlip] = useState(props.flipped);
+
   return (
-    <ReactCardFlip isFlipped={flip}>
+    <ReactCardFlip isFlipped={flip} key={props.image}>
       <button
         style={{
           backgroundColor: "#298932",
           fontSize: "30px",
-          height: "200px",
+          height: "100%",
           width: "200px",
           maxWidth: "100%",
+          minHeight: "150px",
+          minWidth: "100px",
           fontFamily: "dauphinn",
         }}
-        onClick={() => setFlip(true)}
+        onClick={(event) => {
+          if (
+            Math.abs(event.clientX - xPosition.current) < 10 &&
+            Math.abs(event.clientY - yPosition.current) < 10
+          ) {
+            setFlip(true);
+          }
+        }}
+        {...bind}
       >
         ?
       </button>
@@ -24,11 +55,13 @@ export const WorldMapTile = (props: worldMapTileProps) => {
         className="front"
         style={{
           backgroundImage: `url(${props.image})`,
-          backgroundSize: "contain",
+          backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
-          height: "200px",
+          height: "100%",
           width: "200px",
-          // maxWidth: "100%",
+          maxWidth: "100%",
+          minHeight: "150px",
+          minWidth: "100px",
         }}
       ></button>
     </ReactCardFlip>
