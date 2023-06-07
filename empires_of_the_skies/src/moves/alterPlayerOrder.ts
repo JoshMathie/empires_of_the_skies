9ef92b4f46f5207bf5ecab4a6d21596bb41ce7e5
Josh Mathie <joshmathie01@gmail.com>
2023-06-07 11:31:06 +0100

    Moved move functions into separate files to keep things tidier

---
+++ empires_of_the_skies/src/moves/alterPlayerOrder.ts
@@ -0,0 +1,34 @@
+import { MoveFn } from "boardgame.io";
+import { MyGameState } from "../Game";
+import { PlayerOrder } from "../types";
+import { checkCounsellorsNotZero } from "./moveValidation";
+import { INVALID_MOVE } from "boardgame.io/core";
+import { removeOneCounsellor } from "./resourceUpdates";
+
+export const alterPlayerOrder: MoveFn<MyGameState> = (
+  { G, ctx, events, random },
+  ...args
+) => {
+  const newPosition: keyof PlayerOrder = args[1] + 1;
+  const playerID = ctx.currentPlayer;
+  checkCounsellorsNotZero(playerID, G);
+  if (ctx.numPlayers < newPosition) {
+    console.log("Player has chosen a position that is out of bounds");
+    return INVALID_MOVE;
+  }
+  if (G.playerOrder[newPosition] !== undefined) {
+    console.log("Player has chosen a position that is already taken");
+    return INVALID_MOVE;
+  }
+  for (const value of Object.values(G.playerOrder)) {
+    if (value === playerID) {
+      console.log("Player has already altered their position");
+      return INVALID_MOVE;
+    }
+  }
+  removeOneCounsellor(G, playerID);
+  G.boardState.alterPlayerOrder[newPosition] = playerID;
+  G.playerOrder[newPosition] = playerID;
+
+  events.endTurn();
+};
---
+++ empires_of_the_skies/src/moves/discoverTile.ts
@@ -0,0 +1,32 @@
+import { INVALID_MOVE } from "boardgame.io/core/";
+import { MyGameState } from "../Game";
+import { MoveFn } from "boardgame.io";
+
+export const discoverTile: MoveFn<MyGameState> = (
+  { G, ctx, playerID, events, random: RandomAPI },
+  ...args
+) => {
+  const [x, y] = args[1];
+  if (G.mapState.discoveredTiles[y][x] === true) {
+    return INVALID_MOVE;
+  }
+  const boarderingTiles: number[][] = [
+    [x, y - 1 < 0 ? 0 : y - 1],
+    [x, y + 1 > 3 ? 3 : y + 1],
+    [(((x - 1) % 8) + 8) % 8, y],
+    [(((x + 1) % 8) + 8) % 8, y],
+  ];
+  let boardered = false;
+
+  boarderingTiles.forEach((coords) => {
+    if (G.mapState.discoveredTiles[coords[1]][coords[0]] === true) {
+      boardered = true;
+    }
+  });
+  if (boardered === false) {
+    return INVALID_MOVE;
+  }
+  G.mapState.discoveredTiles[y][x] = true;
+
+  events.endTurn();
+};
