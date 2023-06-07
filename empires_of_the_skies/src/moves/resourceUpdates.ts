272b3c440ab8b1f7828fc31be5e3bdd1310cc8b1
Josh Mathie <joshmathie01@gmail.com>
2023-06-07 11:30:34 +0100

    Added generic methods for manipulating resources

---
+++ empires_of_the_skies/src/moves/resourceUpdates.ts
@@ -0,0 +1,36 @@
+import { MyGameState } from "../Game";
+
+export const removeOneCounsellor = (G: MyGameState, playerID: string) => {
+  G.playerInfo[playerID].resources.counsellors -= 1;
+};
+
+export const removeGoldAmount = (
+  G: MyGameState,
+  playerID: string,
+  goldAmount: number
+) => {
+  G.playerInfo[playerID].resources.gold -= goldAmount;
+};
+
+export const addGoldAmount = (
+  G: MyGameState,
+  playerID: string,
+  goldAmount: number
+) => {
+  G.playerInfo[playerID].resources.gold += goldAmount;
+};
+
+export const removeSkyship = (G: MyGameState, playerID: string) => {
+  G.playerInfo[playerID].resources.skyships -= 1;
+};
+export const addSkyship = (G: MyGameState, playerID: string) => {
+  G.playerInfo[playerID].resources.skyships += 1;
+};
+
+export const removeRegiments = (G: MyGameState, playerID: string) => {
+  G.playerInfo[playerID].resources.regiments -= 1;
+};
+
+export const addRegiments = (G: MyGameState, playerID: string) => {
+  G.playerInfo[playerID].resources.regiments += 1;
+};
