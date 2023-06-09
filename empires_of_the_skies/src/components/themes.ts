8d85a33ee89fdc7398650ba0b6c06312d266a3c0
Josh Mathie <joshmathie01@gmail.com>
2023-06-09 11:51:11 +0100

    Added a theme

---
+++ empires_of_the_skies/src/components/themes.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material/styles";
+
+export const generalTheme = createTheme({
+  typography: {
+    fontFamily: "dauphinn",
+    fontSize: 20,
+    button: {
+      textTransform: "none",
+      textAlign: "left",
+      border: "1px solid black",
+    },
+  },
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          whiteSpace: "pre",
+          fontSize: 18,
+        },
+      },
+    },
+  },
+});
